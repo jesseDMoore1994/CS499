e0ea3343505d5fcd5eac926614d686d6333850a2
Matt Eskridge <matt@matteskridge.com>
2016-03-18 02:06:14 -0500

    Added row selector

--- code/src/Controller/PerformancesController.php
+++ code/src/Controller/PerformancesController.php
@@ -39,7 +39,7 @@ class PerformancesController extends AppController {
 
 	}
 
-	public function view($id, $slug) {
+	public function view($id, $slug, $row = 0) {
 
 		$this->viewBuilder()->layout("site");
 		$this->set("css", ["site"]);
@@ -50,6 +50,24 @@ class PerformancesController extends AppController {
 		$this->set("play_date", "January 6th, 2016");
 		$this->set("play_time", "10:00 pm");
 		$this->set("play_about", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra bibendum interdum. Etiam convallis leo quis ipsum rhoncus egestas. Ut a est vel urna cursus feugiat et sagittis enim. Sed lectus arcu, sagittis nec urna in, ultrices hendrerit massa. Quisque rutrum efficitur scelerisque. Fusce non convallis ex, sagittis elementum eros. Suspendisse mattis, velit vel pellentesque varius, felis leo gravida magna, vel dignissim arcu ipsum vel diam. Etiam suscipit felis mi, at facilisis tellus pulvinar et.");
+
+		$this->set("selected_row", $row);
+
+		$this->set("sections", [
+			["Front", "Back", [
+				["A", 0, ["A1", "A2", "A3", "A4", "A5"], [false, false, false, false, false]],
+				["B", 2, ["B1", "B2", "B3", "B4", "B5"], [true, true, false, false, false]],
+				["C", 5, ["C1", "C2", "C3", "C4", "C5"], [true, true, true, true, true]],
+			]],
+			["Balcony", "&nbsp;", [
+				["U", 5, ["U1", "U2", "U3", "U4", "U5"], [true, true, true, true, true]],
+				["V", 5, ["V1", "V2", "V3", "V4", "V5"], [true, true, true, true, true]],
+				["W", 5, ["W1", "W2", "W3", "W4", "W5"], [true, true, true, true, true]],
+			]],
+			["&nbsp;", "&nbsp;", [
+				["<span class='icomoon'>&#xe9b2;</span>", 5, ["AC1", "AC2", "AC3", "AC4", "AC5"], [true, true, true, true, true]],
+			]]
+		]);
 
 	}
 
--- code/src/Template/Performances/view.ctp
+++ code/src/Template/Performances/view.ctp
@@ -30,7 +30,31 @@
 				</p>
 			</div>
 			<div class="site-performance-rows">
+				<?php $j = 0; foreach ($sections as $sec) { ?>
+				<div class="site-performance-rows-section">
+					<div class="site-perfrowsec-right">
+						<?= $sec[1] ?>
+					</div>
+					<div class="site-perfrowsec-left">
+						<?= $sec[0] ?>
+					</div>
+					<div class="site-prefrowsec-rows">
+						<?php for ($i = 0; $i < count($sec[2]); $i++) {
+						?><div class="site-prefrowsec-row<?php if ($selected_row == $j) echo " selected"; ?>">
+							<div class="site-prefrowsec-rowid">
+								<a href=""><?= $sec[2][$i][0] ?></a>
+							</div>
+							<div class="site-prefrowsec-seats <?php
+								if ($sec[2][$i][1] == 0) { echo "bad"; }
+								else if ($sec[2][$i][1] < 3) { echo "warning"; }
+								else { echo "good"; } ?>">
 
+								<a href=""><?= $sec[2][$i][1] ?></a>
+							</div>
+						</div><?php $j++; } ?>
+					</div>
+				</div>
+				<?php } ?>
 			</div>
 			<div class="site-performance-seats">
 
